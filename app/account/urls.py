2b12f2103721ef167dcf6224da665c2b888a19b2
ftumj259 <2013470259@ftumj.ac.id>
2017-02-27 22:48:34 +0700

    Getting started My Directory Project

---
+++ app/account/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+from . import views
+
+app_name = 'account'
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    # url(r'^edit/(?P<id>\d+)/$', views.edit, name='edit'),
+    # url(r'^delete/(?P<id>\d+)/$', views.delete, name='delete'),
+    # url(r'^detail/(?P<id>\d+)/$', views.detail, name='detail'),
+    # url(r'^new/$', views.new, name='new'),
+    # url(r'^create/$', views.create, name='create'),
+]
